f6e7c3e41f79b0d78048a72bea1571ef06b13b52
Daniil Voronovych <daniil_voronovych@epam.com>
2026-01-22 00:56:26 +0200

    task1-8: Add parameters to the model requests

--- task/1-task-models.py
+++ task/1-task-models.py
@@ -12,7 +12,10 @@ from task.app.main import run
 # - gemini-2.5-pro
 
 run(
-    deployment_name='INSERT_DEPLOYMENT_NAME',
+     deployment_name='gpt-4o',
+   # deployment_name='claude-3-7-sonnet@20250219',
+   # deployment_name='gemini-2.5-pro',
+
     print_request=False, # Switch to False if you do not want to see the request in console
     print_only_content=False, # Switch to True if you want to see only content from response
 )
--- task/2-task-n.py
+++ task/2-task-n.py
@@ -13,7 +13,14 @@ from task.app.main import run
 run(
     # TODO:
     #  1. Provide `deployment_name` with model from the list above👆
+    deployment_name='gpt-4o',
+   # deployment_name='claude-3-7-sonnet@20250219',
+   # deployment_name='gemini-2.5-pro',
+
+    print_request=False, # Switch to False if you do not want to see the request in console
+    print_only_content=False, # Switch to True if you want to see only content from response
     #  2. Use `n` parameter with value in range from 1 to 5!
+    n=5
 )
 
 # Pay attention to the number of choices in the response!
